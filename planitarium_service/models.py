c1a849fa441f4fa3f4decab461ed2da0278f1d5b
Hamster <dtaranenko137@gmail.com>
2025-09-13 13:54:12 +0300

    add models

--- planitarium_service/models.py
+++ planitarium_service/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255)
+    shows = models.ManyToManyField(AstronomyShow, related_name="themes")
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seat_in_row = models.IntegerField()
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE, related_name="show_sessions")
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE, related_name="dome_sessions")
+    show_time = models.DateTimeField()
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reservation")
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE, related_name="tickets")
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="tickets")
+
+    class Meta:
+        unique_together = ('show_session', 'row', 'seat')
+
+    @staticmethod
+    def validate_seat(seat: int, num_seats: int, error_to_raise):
+        if not (1 <= seat <= num_seats):
+            raise error_to_raise({
+                "seat": f"seat must be in range [1, {num_seats}], not {seat}"
+            })

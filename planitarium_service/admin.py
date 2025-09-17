5787fc1c211784f4b6a74a5c5964b119caccfe15
Hamster <dtaranenko137@gmail.com>
2025-09-17 11:00:22 +0300

    add models to admin

--- planitarium_service/admin.py
+++ planitarium_service/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from planitarium_service.models import (
+    AstronomyShow,
+    ShowTheme,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
+    Ticket,
+)
 
-# Register your models here.
+admin.site.register(AstronomyShow)
+admin.site.register(ShowTheme)
+admin.site.register(PlanetariumDome)
+admin.site.register(ShowSession)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
